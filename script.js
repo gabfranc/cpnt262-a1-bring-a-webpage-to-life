27277fe57c5f6e2d63ff417a8a73af32dce08617
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-22 22:35:22 -0700

    import images and arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//DONE: add arrays for Image carousel
+
+const imgCarousel = ["./Images/fashionModel.jpg", "./Images/model_One.jpg", "./Images/modelOnRunway.jpg", "./Images/Runway.jpeg", "./Images/runwayImage.jpeg"];
+
+
+
